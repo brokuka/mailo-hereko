e15fc0b61b17dac923f9f274fba7bdf5cb5c5bcf
brokuka <75063653+brokuka@users.noreply.github.com>
2022-08-05 22:40:40 +0400

    Разработка компонента для инпута (#9)
    * add input component
    
    * fix preview and adding width 100%

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,62 @@
+import { useRef, useEffect, useCallback } from "react";
+import Input from "../components/Input/Input";
 import Container from "./../components/Container/Container";
 
 export default function Home() {
+  const ref = useRef();
+
+  useEffect(() => {
+    const el = ref.current;
+
+    console.log("ref:", el);
+  }, []);
+
+  const customStyles = {
+    maxWidth: "344px",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    margin: "2rem auto",
+    gap: "2rem",
+  };
+
   return (
     <Container>
-      <h1>Heading One</h1>
+      <div style={customStyles}>
+        <h2>Variant #1</h2>
+        <Input
+          type="text"
+          id="test"
+          label="Variant #1"
+          placeholder="eg. Avenger"
+          ref={ref}
+        />
+        <h2>Variant #2</h2>
+        <Input
+          type="email"
+          id="test"
+          label="Variant #2"
+          placeholder="eg. Films"
+          icon="left"
+        />
+        <h2>Variant #3</h2>
+        <Input
+          type="email"
+          id="test"
+          label="Variant #2"
+          placeholder="eg. Films"
+          icon="right"
+        />
+        <h2>Variant #4</h2>
+        <Input
+          type="email"
+          id="test"
+          label="Variant #2"
+          placeholder="eg. Films"
+          icon="both"
+        />
+      </div>
     </Container>
   );
 }
