c03674783bf9e11037181d19f518dc3cf1e739f1
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2022-08-05 22:39:16 +0400

    fix preview and adding width 100%

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,40 +11,52 @@ export default function Home() {
     console.log("ref:", el);
   }, []);
 
+  const customStyles = {
+    maxWidth: "344px",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    margin: "2rem auto",
+    gap: "2rem",
+  };
+
   return (
     <Container>
-      <h2>Variant #1</h2>
-      <Input
-        type="text"
-        id="test"
-        label="Variant #1"
-        placeholder="eg. Avenger"
-        ref={ref}
-      />
-      <h2>Variant #2</h2>
-      <Input
-        type="email"
-        id="test"
-        label="Variant #2"
-        placeholder="eg. Films"
-        icon="left"
-      />
-      <h2>Variant #3</h2>
-      <Input
-        type="email"
-        id="test"
-        label="Variant #2"
-        placeholder="eg. Films"
-        icon="right"
-      />
-      <h2>Variant #4</h2>
-      <Input
-        type="email"
-        id="test"
-        label="Variant #2"
-        placeholder="eg. Films"
-        icon="both"
-      />
+      <div style={customStyles}>
+        <h2>Variant #1</h2>
+        <Input
+          type="text"
+          id="test"
+          label="Variant #1"
+          placeholder="eg. Avenger"
+          ref={ref}
+        />
+        <h2>Variant #2</h2>
+        <Input
+          type="email"
+          id="test"
+          label="Variant #2"
+          placeholder="eg. Films"
+          icon="left"
+        />
+        <h2>Variant #3</h2>
+        <Input
+          type="email"
+          id="test"
+          label="Variant #2"
+          placeholder="eg. Films"
+          icon="right"
+        />
+        <h2>Variant #4</h2>
+        <Input
+          type="email"
+          id="test"
+          label="Variant #2"
+          placeholder="eg. Films"
+          icon="both"
+        />
+      </div>
     </Container>
   );
 }
