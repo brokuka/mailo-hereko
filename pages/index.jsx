4c92e710eacc32b78876615186209e7a46782ea7
brokuka <75063653+brokuka@users.noreply.github.com>
2022-08-18 18:36:27 +0400

    add component (#18)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,12 @@
-import Button from "../components/Button/Button";
 import Card from "../components/Card/Card";
-import Header from "../components/Header/Header";
-import Input from "../components/Input/Input";
-import Row from "../components/Row/Row";
-import Spinner from "../components/Spinner/Spinner";
+import Rating from "../components/Rating/Rating";
 import Container from "./../components/Container/Container";
 
 export default function Home() {
   return (
     <Container>
-      <Header variant="auth" />
-      {/* <h2>Non auth</h2>
-      <Header variant="nonAuth" /> */}
+      <Card />
+      <Rating value={5} />
     </Container>
   );
 }
